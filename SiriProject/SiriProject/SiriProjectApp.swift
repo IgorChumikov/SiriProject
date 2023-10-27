2cd9f71bb72ec131d938a390caaaa045e82df224
Igor <chumikov.igor@mail.ru>
2023-10-27 19:52:26 +0700

    Add SiriProject

---
+++ SiriProject/SiriProject/SiriProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriProjectApp.swift
+//  SiriProject
+//
+//  Created by Игорь Чумиков on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SiriProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
